e058a4eea72e561b547ec46ae80cb53013edc26c
shafique  ahmed <drshafiquemahar18@gmail.com>
2025-11-10 18:45:49 +0500

    feat:function

---
+++ app.js
@@ -0,0 +1,95 @@
+
+// this is the simple grade check program
+
+
+
+// console.log("HELLO WORLD");
+// alert('HELLO')
+// const students = ['zain', 'shafique', 'muzamil'];
+// console.table(students);
+// // this is the simple age check program
+// let age = 2;
+// if (age >= 18) {
+//     console.log('Adult');
+// } else {
+//     console.log('Minor');
+// }
+
+
+
+// this is the simple grade check program
+
+// let marks = 85;
+// if (marks > 90) console.log('A');
+// else if (marks >= 80) console.log('B');
+// else consloe.log('C')
+
+
+
+
+// this is the simple GRADS SYSTEM check program
+// This is very IMPORTANYT TOPIC SO BE CARE FULL IN TIS TOPIC
+
+// let marks = prompt("please inter your marks:");
+// if (marks < 0 || marks > 100) {
+//     alert('invalid mark');
+// } else if (marks >= 90) {
+//     alert("congratulation! you  got A grade.");
+// } else if (marks >= 80){
+//     alert("hurray! you got A+ Grade");
+// } else if (marks >= 70){
+//     alert("you got A Grade");
+// }
+
+
+
+
+
+// this is the simple COUNTING check program
+
+// for(let i=1; i<=5; i++){
+//     console.log('count:',i);
+// }
+
+
+
+// this is the simple grade check program
+// let count= 1;
+// while(count<= 3 ){
+//       console.log(count);count ++;
+// }
+// let num = 1;
+// do{
+//     console.log(num);
+//     num ++ ;
+//  } while(num <= 3);
+
+
+
+
+
+// for(let i=1;i<=18;++i){
+//     if(i===5) continue;
+//     console.log('number:',i);
+// }
+
+
+
+
+
+
+// function greet(){
+//     console.log("hello!");
+//     console.log("assalamoalaikum");
+//     console.log("HELLO!");
+//     console.log("HELLO");
+// }
+
+// greet();
+
+
+function add(a,b){
+    return a + b;
+}
+ let sum =add(5,3);
+ console.log(sum);
